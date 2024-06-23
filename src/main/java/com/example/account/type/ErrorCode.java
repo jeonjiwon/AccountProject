aedd1a5a115001b5d08b4850800e62cdde827fe3
wjseldnjs <wjseldnjs@douzone.com>
2024-06-24 02:31:06 +0900

    잔액 생성 구현, 컨트롤러 테스트

--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
     
     ACCOUNT_NOT_FOUND("계좌가 없습니다. "),
 
+    AMOUNT_EXCEED_BALANCE("거래 금액이 계좌 잔액보다 큽니다. "),
+
     USER_ACCOUNT_UN_MATCH("사용자와 계좌의 소유자가 다릅니다. "),
 
     ACCOUNT_ALREADY_UNREGISTED("계좌가 이미 해지되었습니다."),
