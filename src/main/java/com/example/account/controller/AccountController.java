d8640eb76d26cad10a43a0656cf2e4482a0df64f
wjseldnjs <wjseldnjs@douzone.com>
2024-06-24 00:52:08 +0900

    계좌 생성, 해지, 조회 관련

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -3,10 +3,15 @@ package com.example.account.controller;
 import com.example.account.domain.Account;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
+import com.example.account.dto.AccountInfo;
+import com.example.account.dto.CreateAccount;
+import com.example.account.dto.DeleteAccount;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,15 +20,45 @@ public class AccountController {
 
     private final RedisTestService redisTestService;
 
+    @PostMapping("/account")
+    public CreateAccount.Response createAccount(
+            @RequestBody @Valid CreateAccount.Request request){
+
+        return CreateAccount.Response.from(
+                accountService.createAccount(
+                    request.getUserId(),
+                    request.getInitialBalance())
+        );
+    }
+
+    @DeleteMapping("/account")
+    public DeleteAccount.Response DeleteAccount(
+            @RequestBody @Valid DeleteAccount.Request request){
+
+        return DeleteAccount.Response.from(
+                accountService.DeleteAccount(
+                    request.getUserId(),
+                    request.getAccountNumber())
+        );
+    }
+
+    @GetMapping("/account")
+    public List<AccountInfo> getAccountsByUserId(
+            @RequestParam("user_id") Long user_id
+    ){
+        return accountService.getAccountsByUserId(user_id)
+                .stream().map(accountDto ->
+                        AccountInfo.builder()
+                        .accountNumber(accountDto.getAccountNumber())
+                        .balance(accountDto.getBalance())
+                        .build())
+                .collect(Collectors.toList());
+
+    }
+
     @GetMapping("/get-lock")
     public String getLock(){
         return redisTestService.getLock();
-    }
-
-    @GetMapping("/create-account")
-    public String createAccount(){
-        accountService.createAccount();
-        return "success";
     }
 
     @GetMapping("/account/{id}")
--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -1,29 +1,115 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
-import com.example.account.domain.AccountStatus;
+import com.example.account.domain.AccountUser;
+import com.example.account.exception.AccountException;
 import com.example.account.repository.AccountRepository;
+import com.example.account.repository.AccountUserRepository;
+import com.example.account.type.AccountStatus;
+import com.example.account.type.ErrorCode;
+import com.example.account.dto.AccountDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final AccountUserRepository accountUserRepository;
 
+    /**
+     * 사용자가 있는지 조회
+       계좌번호를 생성하고
+       계좌를 저장하고, 그 정보를 넘긴다
+     * @param userId
+     * @param InitialBalance
+     */
     @Transactional
-    public void createAccount(){
-        Account account = Account.builder()
-                .accountNumber("40000")
-                .accountStatus(AccountStatus.IN_USE)
-                .build();
+    public AccountDto createAccount(Long userId, Long InitialBalance){
+       AccountUser accountUser = accountUserRepository.findById(userId)
+               .orElseThrow(()-> new AccountException(ErrorCode.USER_NOT_FOUND));
 
-        accountRepository.save(account);
+       validateCreateAccount(accountUser);
+
+       String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
+               .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "")
+               .orElse("1000000000");
+
+       return AccountDto.fromEntity(
+               accountRepository.save(Account.builder()
+                       .accountUser(accountUser)
+                       .accountStatus(AccountStatus.IN_USE)
+                       .accountNumber(newAccountNumber)
+                       .balance(InitialBalance)
+                       .registeredAt(LocalDateTime.now())
+                       .build())
+               );
+    }
+
+    private void validateCreateAccount(AccountUser accountUser) {
+       if(accountRepository.countByAccountUser(accountUser) == 10) {
+            throw new AccountException(ErrorCode.MAX_ACCOUNT_PER_USER_10);
+       }
     }
 
     @Transactional
     public Account getAccount(Long id) {
+        if(id < 0 ){
+            throw new RuntimeException("Minus");
+        }
         return accountRepository.findById(id).get();
+    }
+
+
+    @Transactional
+    public AccountDto DeleteAccount(Long userId, String accountNumber) {
+
+       AccountUser accountUser = accountUserRepository.findById(userId)
+               .orElseThrow(()-> new AccountException(ErrorCode.USER_NOT_FOUND));
+       Account account = accountRepository.findByAccountNumber(accountNumber)
+               .orElseThrow(()-> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+       validateDeleteAccount(accountUser, account);
+
+       account.setAccountStatus(AccountStatus.UNREGISTERED);
+       account.setUnregisteredAt(LocalDateTime.now());
+
+       // 없어도 동작하긴 함
+        accountRepository.save(account);
+
+
+       return AccountDto.fromEntity(account);
+    }
+
+    private void validateDeleteAccount(AccountUser accountUser, Account account) {
+        if(!Objects.equals(accountUser.getId(),account.getAccountUser().getId())) {
+            throw new AccountException(ErrorCode.USER_ACCOUNT_UN_MATCH);
+        }
+
+        if(account.getAccountStatus() == AccountStatus.UNREGISTERED) {
+            throw new AccountException(ErrorCode.ACCOUNT_ALREADY_UNREGISTED);
+        }
+
+        if(account.getBalance() > 0) {
+            throw new AccountException(ErrorCode.BALANCE_NOT_EMPTY);
+        }
+
+    }
+
+
+    @Transactional
+    public List<AccountDto> getAccountsByUserId(Long userId) {
+
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        List<Account> accounts = accountRepository.findByAccountUser(accountUser);
+
+        return accounts.stream().map(AccountDto::fromEntity).collect(Collectors.toList());
     }
 }
